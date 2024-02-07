df889eb44939c4ae223bfebdc62b99c2500b2dc7
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-08 01:09:34 +0530

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,24 +22,24 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
         }
         else
         {
-            binary_tree_t *temp = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+            binary_tree_t temp; /**= (binary_tree_t*)malloc(sizeof(binary_tree_t)) **/
             if (temp == NULL)
             {
                 return NULL;
             }
-            temp->parent = NULL;
-            temp->right = NULL;
-            temp->left = NULL;
-            temp->n = parent->left->n;
+            temp.parent = NULL;
+            temp.right = NULL;
+            temp.left = NULL;
+            temp.n = parent->left->n;
             parent->left = binary_tree_node(parent, value);
             if (parent->left == NULL) {
-                free(temp);
+                /** free(temp); **/
                 return NULL;
             }
             /** temp->parent = parent; **/
             /** parent->left->->left->parent = parent; **/
-            binary_tree_insert_left(parent->left,temp->n);
-            free(temp);
+            binary_tree_insert_left(parent->left,temp.nn);
+            /**free(temp); **/
             return (parent);
         }
     }
