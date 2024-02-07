eb0c56bfe64b7cddbb2f8102c7b2bd47434f0a1d
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-08 01:38:12 +0530

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,11 +12,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     {
         return (NULL);
     }
-    binary_tree_t new_node;
     else
     {
         /**binary_tree_t *new_node = binary_tree_node(parent, value); **/
-        
+        binary_tree_t new_node;
+        binary_tree_t *new_node_p = &new_node;
         new_node.parent = parent;
         new_node.right = NULL;
         new_node.left = NULL;
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
         if(parent->left == NULL)
         {
             parent->left = &new_node;
-            return (&new_node);
+            return (&new_node_p);
         }
         else
         {
