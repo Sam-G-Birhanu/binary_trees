e9f0e81ff67885edebbd16f52a41a760da3370dc
Sam-G-Birhanu <21btrcs228@jainuniversity.ac.in>
2024-02-08 05:47:15 -0800

    committed

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,19 +7,19 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-  if (node == NULL)
-  {
-    return (0);
-  }
-  else
-  {
-    if (node->left == NULL && node->right == NULL)
-    {
-      return (1);
-        }
-    else
-    {
-      return (0);
-  }
-}
+	if (node == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (node->left == NULL && node->right == NULL)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
 }
