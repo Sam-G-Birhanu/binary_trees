b2970dc55f6fc89cdfb519f1f6f4e74a9ea0d042
Sam-G-Birhanu <21btrcs228@jainuniversity.ac.in>
2024-02-08 05:33:57 -0800

    committed

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,21 +1,26 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_delete - a function that deletes a tree
+ * @tree: a pointer to the tree to be deleted
+ * Return: void
+ **/
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-  if (tree == NULL)
-  {
-    return;
-  }
-  else
-  {
-    if (tree-> left != NULL)
-    {
-      binary_tree_delete(tree-> left);
-    }
-    if (tree-> right != NULL)
-    {
-      binary_tree_delete(tree->right);
-    }
-    free(tree);
-  }
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree->left != NULL)
+		{
+			binary_tree_delete(tree->left);
+		}
+		if (tree->right != NULL)
+		{
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
 }
