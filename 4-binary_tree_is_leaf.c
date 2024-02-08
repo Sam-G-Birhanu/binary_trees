64a04ddd5254bf0a2db130f01500d223b7635aec
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-08 19:14:45 +0530

    Update 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,11 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-  if (node)
+  if (node == NULL)
+  {
+    return (0);
+  }
+  else
   {
     if (node->left == NULL && node->right == NULL)
     {
