571dc996049a6b3a7b5c4bf1f65dfc7ec6624362
Sam-G-Birhanu <21btrcs228@jainuniversity.ac.in>
2024-02-08 05:04:16 -0800

    fixed betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,35 +2,37 @@
 
 /**
  * binary_tree_insert_left - a function that inserts a node on the left
- * @parent: the parent node in which the new node is being added to 
+ * @parent: the parent node in which the new node is being added to
  * @value: the value of the new node being inserted
  * Return: It returns the new node that has been created
  **/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if(parent == NULL)
-    {
-        return (NULL);
-    }
-    else
-    {
-        binary_tree_t *new_node = binary_tree_node(parent, value);
-        if(parent->left == NULL)
-        {
-            parent->left = new_node;
-            return (new_node);
-        }
-        else
-        {
-            binary_tree_t *temp = binary_tree_node(NULL,parent->left->n );
-            binary_tree_t *new_node_2 = binary_tree_node(new_node, temp->n);
-            int x = new_node_2->n;
-            free(parent->left);
-            parent->left = new_node;
-            binary_tree_insert_left(new_node,x);
-            free(temp);
-            free(new_node_2);
-            return (new_node);
-        }
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		binary_tree_t *new_node = binary_tree_node(parent, value);
+
+		if (parent->left == NULL)
+		{
+			parent->left = new_node;
+			return (new_node);
+		}
+		else
+		{
+			binary_tree_t *temp = binary_tree_node(NULL, parent->left->n);
+			binary_tree_t *new_node_2 = binary_tree_node(new_node, temp->n);
+			int x = new_node_2->n;
+
+			free(parent->left);
+			parent->left = new_node;
+			binary_tree_insert_left(new_node, x);
+			free(temp);
+			free(new_node_2);
+			return (new_node);
+		}
+	}
 }
