40b2d3bd1822cafcf484489c33c195a6eaaf9d52
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-08 18:56:16 +0530

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,6 +8,14 @@ void binary_tree_delete(binary_tree_t *tree)
   }
   else
   {
+    if (tree-> left != NULL)
+    {
+      binary_tree_delete(tree-> left);
+    }
+    if (tree-> right != NULL)
+    {
+      binary_tree_delete(tree->right);
+    }
     free(tree);
   }
 }
