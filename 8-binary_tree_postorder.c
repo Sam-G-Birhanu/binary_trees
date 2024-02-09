afa97ecbd3d41ffdad9fd8133c4fdd4f12955321
Sam-G-Birhanu <21btrcs228@jainuniversity.ac.in>
2024-02-09 04:51:56 -0800

    added file

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,8 +2,8 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_postorder(tree->left);
+	binary_tree_postorder(tree->left, func);
 	func(tree->n);
-	binary_tree_postorder(tree->right);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
