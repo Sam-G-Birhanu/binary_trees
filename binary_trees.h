2f88ca0529a930db28e61e7774cd1c097ff8954a
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 01:44:16 +0530

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -34,5 +34,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t count_right(const binary_tree_t *tree, int count);
 size_t count_left(const binary_tree_t *tree, int count);
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 #endif
