c776359632dcf63e41beb2c3f1b04d86fb4ee9a5
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 01:09:05 +0530

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,1 +1,34 @@
 #include "binary_trees.h"
+
+size_t count_right(binary_tree_t *tree, int count)
+{
+    if (tree == NULL)
+        return count;
+  if (tree->right != NULL) {
+        count++;
+        count = count_right(tree->right, count);
+    }
+return count;
+}
+
+size_t count_left(binary_tree_t *tree, int count)
+{
+    if (tree == NULL)
+        return count;
+    if (tree->left != NULL) {
+        count++;
+        count = count_left(tree->left, count);
+    }
+
+    return (count);
+}
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+  size_t left_h = count_left(tree);
+  size_t right_h = count_right(tree);
+  if (left > right)
+    return (left);
+  else
+    return (right)
+}
