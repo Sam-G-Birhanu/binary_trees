bb90489cd59afa7f2adc29e623546302005d0817
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 01:52:06 +0530

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,1 +1,16 @@
 #include "binary_trees.h"
+
+size_t get_depth(const binary_tree_t *tree, size_t count)
+{
+    if (tree == NULL)
+        return count;
+  if (tree->parent != NULL) {
+        count++;
+        count = get_depth(tree->parent, count);
+    }
+return count;
+}
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    return get_depth(tree, 0);
+}
