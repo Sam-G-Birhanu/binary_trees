7ee4bb7f2290f9afcd030fbe2790e02614d07030
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 14:41:10 +0530

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,11 +17,12 @@ size_t max(size_t a, size_t b)
  **/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    size_t left_height;
+    size_t right_height;
     if (tree == NULL)
         return 0;
-
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
     return max(left_height, right_height) + 1;
 }
