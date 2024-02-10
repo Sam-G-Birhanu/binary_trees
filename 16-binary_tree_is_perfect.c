1cb9892c94da5c202f0774c5810dcd62eea392c5
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 20:54:39 +0530

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,7 +47,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 if (tree == NULL)
   return (0);
-if ( binary_tree_balance(tree) == 1 || binary_tree_balance(tree) == 0)
+if (binary_tree_balance(tree) == 0)
   return (1);
 return (0);
 }
