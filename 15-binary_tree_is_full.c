1c83a409f0e503d530687406860b4c30f7f394b9
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 15:47:18 +0530

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,12 +3,9 @@
 int binary_tree_is_full(const binary_tree_t *tree){
 int f = 1;
 if (tree == NULL)
-  return 0;
+  return 1;
 if ((tree->left == NULL && tree->right == NULL) || (tree->left != NULL && tree->right != NULL))
-    f = (1);
-  else
-     f = 0;
-    f = f * binary_tree_is_full(tree->left);
-    f = f * binary_tree_is_full(tree->left);
-    return (f);
+      return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+
+    return (0);
 }
