337084aef903e354c722c5f181f32ae8c1a08c45
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 15:03:11 +0530

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,50 +1,45 @@
 #include "binary_trees.h"
-/**
- * max - returns the maximum of two numbers
- * @a: first number
- * @b: second number
- * Return: maximum of a and b
- **/
-size_t max(size_t a, size_t b)
-{
-    return (a > b ? a : b);
-}
 
 /**
- * binary_tree_height - computes the height of a binary tree
- * @tree: root node of the binary tree
- * Return: height of the binary tree
- **/
-size_t binary_tree_h(const binary_tree_t *tree)
-{
-    size_t left_height;
-    size_t right_height;
-    if (tree == NULL)
-        return 0;
-    left_height = binary_tree_h(tree->left);
-    right_height = binary_tree_h(tree->right);
-
-    return max(left_height, right_height) + 1;
-}
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-    size_t h;
-    if (tree == NULL)
-        return (0);
-    h = binary_tree_h(tree) -1;
-    return (h);
-}
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure
+ * Return: balance factor
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-  int b_f;
-  int l;
-  int r;
-  if (tree == NULL)
-  {
-    return 0;
-  }
-  l = (int)binary_tree_height(tree->left);
-  r = (int)binary_tree_height(tree->right);
-  b_f = l - r;
-  return b_f;
+	int balance;
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	left_height = measure_height(tree->left);
+	right_height = measure_height(tree->right);
+
+	balance = left_height - right_height;
+
+	return (balance);
+}
+
+/**
+ * measure_height - measures height of a binary tree recursively
+ * @tree: pointer to the root of the tree to measure
+ * Return: height
+ */
+size_t measure_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = measure_height(tree->left);
+	right_height = measure_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
