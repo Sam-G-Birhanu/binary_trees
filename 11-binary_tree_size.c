3ce504ca741d8ce4a6efef66d476491ac0f0f221
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 10:55:29 +0530

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,1 +1,15 @@
 #include "binary_trees.h"
+
+size_t binary_tree_count(const binary_tree_t *tree,size_t count)
+{
+	if (tree == NULL)
+		return count;
+	count = binary_tree_count(tree->left, count);
+	count++;
+	count = binary_tree_count(tree->right, count);
+	return count;
+}
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+return binary_tree_count(tree, 0);
+}
