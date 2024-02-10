f0cca11bce7b4ba49647dd8ab8608bd7dab39426
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 14:04:18 +0530

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,56 @@
 #include "binary_trees.h"
-size_t binary_tree_height(const binary_tree_t *tree);
+/**
+ * count_right - counts nodes on the left
+ * @tree: the tree to be traveresed
+ * @count: counts the number of nodes
+ * Return: returns count
+ **/
 
+size_t count_right(const binary_tree_t *tree, int count)
+{
+	if (tree == NULL)
+		return (count);
+	if (tree->right != NULL)
+	{
+		count++;
+		count = count_right(tree->right, count);
+	}
+	return (count);
+}
+/**
+ * count_left - counts nodes on the left
+ * @tree: the tree to be traveresed
+ * @count: counts the number of nodes
+ * Return: returns count
+ **/
+size_t count_left(const binary_tree_t *tree, int count)
+{
+	if (tree == NULL)
+		return (count);
+	if (tree->left != NULL)
+	{
+		count++;
+		count = count_left(tree->left, count);
+	}
+	return (count);
+}
+/**
+ * binary_tree_height- gets the height of tree
+ * @tree: root node of the tree to measure the height.
+ * Return: returns height
+ **/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	const binary_tree_t *temp = tree;
+	size_t left_h = count_left(temp, 0);
+	size_t right_h = count_right(temp, 0);
+
+	if (left_h > right_h)
+		return (left_h);
+	else
+		return (right_h);
+}
 int binary_tree_balance(const binary_tree_t *tree)
 {
   int b_f;
