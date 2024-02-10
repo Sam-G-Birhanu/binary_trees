155a38fe733c4c4251dbaf6fef3ad51625f1113b
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 11:26:08 +0530

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,1 +1,15 @@
 #include "binary_trees.h"
+size_t binary_tree_count_l(const binary_tree_t *tree,size_t count)
+{
+	if (tree == NULL)
+		return count;
+	count = binary_tree_count_l(tree->left, count);
+	if (tree->left == NULL && tree->right == NULL)
+    	count++;
+	count = binary_tree_count_l(tree->right, count);
+	return count;
+}
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+  return binary_tree_count(tree, 0);
+}
