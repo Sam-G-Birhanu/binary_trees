6208fee053714827ca84df3a6dfb5922d341775f
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 11:36:11 +0530

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,7 +10,7 @@ size_t count_nl(const binary_tree_t *tree,size_t count)
 	count = count_nl(tree->right, count);
 	return count;
 }
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 return count_nl(tree, 0);
 }
