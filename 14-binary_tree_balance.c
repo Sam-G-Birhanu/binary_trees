0259e5a2a4eb8b3a2efdf7a1b27f919592631e0b
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 11:51:49 +0530

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 int binary_tree_balance(const binary_tree_t *tree)
 {
-  if (tree == NUlL)
+  if (tree == NULL)
     return NULL;
   int b_f = int(binary_tree_height(tree->left) - binary_tree_height(tree->right));
   return b_f;
