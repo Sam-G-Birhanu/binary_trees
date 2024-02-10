daa5cc02748cea59e592cbe38695b3c24403f4d6
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 22:05:55 +0530

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -5,9 +5,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
   if (node != NULL)
 {
 if (node->parent->left == node)
-  return (parent->right);
+  return (node->parent->right);
 else
-  return (parent->left);
+  return (node->parent->left);
 }
   else
     return (NULL);
