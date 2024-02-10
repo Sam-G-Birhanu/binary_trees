e5ca8acdd348f40baba025488f1e12e0f2cfd472
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 22:17:15 +0530

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,7 +2,7 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-  if (node != NULL && if node->parent != NULL)
+  if (node != NULL && node->parent != NULL)
 {
 if (node->parent->left == node)
   return (node->parent->right);
@@ -15,7 +15,7 @@ else
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (node != NULL && if node->parent != NULL)
+if (node != NULL && node->parent != NULL)
   return binary_tree_sibling(node->parent);
 else
   return (NULL);
