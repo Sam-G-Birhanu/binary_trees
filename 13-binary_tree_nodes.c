5e4666687e28b36df5e5b2f098a386efcaf04dee
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 11:34:22 +0530

    Create 13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+size_t count_nl(const binary_tree_t *tree,size_t count)
+{
+	if (tree == NULL)
+		return count;
+	count = count_nl(tree->left, count);
+	if (tree->left != NULL || tree->right != NULL)
+    	count++;
+	count = count_nl(tree->right, count);
+	return count;
+}
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+return count_nl(tree, 0);
+}
