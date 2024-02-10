2dc2003ef3d8be5f4cae74b87f7772672352f035
Sam-G-Birhanu <21btrcs228@jainuniversity.ac.in>
2024-02-09 21:31:37 -0800

    fixed betty style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,15 +1,27 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_count: function that counts nodes
+ * @tree: tree to be traversed
+ * @count: stores counted nodes
+ * Return: returns node count
+ **/
 
 size_t binary_tree_count(const binary_tree_t *tree,size_t count)
 {
 	if (tree == NULL)
-		return count;
+		return (count);
 	count = binary_tree_count(tree->left, count);
 	count++;
 	count = binary_tree_count(tree->right, count);
-	return count;
+	return (count);
 }
+/**
+ * binary_tree_size- functions that returns size
+ * @tree: tree to be traversed
+ * Return: returns size
+ **/
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-return binary_tree_count(tree, 0);
+	return binary_tree_count(tree, 0);
 }
