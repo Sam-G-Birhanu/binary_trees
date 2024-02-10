309f6fa275ce21ab887bf64d1c0a5135d6dc27c9
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-02-10 21:14:53 +0530

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -42,12 +42,39 @@ size_t measure_height(const binary_tree_t *tree)
 	else
 		return (right_height + 1);
 }
+/**
+ * binary_tree_count - function that counts nodes
+ * @tree: tree to be traversed
+ * @count: stores counted nodes
+ * Return: returns node count
+ **/
 
+size_t binary_tree_count(const binary_tree_t *tree, size_t count)
+{
+	if (tree == NULL)
+		return (count);
+	count = binary_tree_count(tree->left, count);
+	count++;
+	count = binary_tree_count(tree->right, count);
+	return (count);
+}
+/**
+ * binary_tree_size - functions that returns size
+ * @tree: tree to be traversed
+ * Return: returns size
+ **/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	return (binary_tree_count(tree, 0));
+}
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t h = measure_height(tree);
 if (tree == NULL)
   return (0);
-if (binary_tree_balance(tree) == 0)
+
+if (binary_tree_balance(tree) == 0 && binary_tree_size(tree) == ((2 ** (h + 1)) - 1))
   return (1);
-return binary_tree_size(tree);
+return 0;
 }
