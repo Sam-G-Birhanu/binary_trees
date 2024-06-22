5706ca89a17d8511cc6915ba371ac69d81efcc80
root <21btrcs228@jainuniversity.ac.in>
2024-06-22 03:54:56 -0700

    count_nl

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,15 +1,28 @@
 #include "binary_trees.h"
-size_t binary_tree_count_l(const binary_tree_t *tree,size_t count)
+/**
+ * binary_tree_count_l - function that counts
+ * @binary_tree_t *tree: a parameter
+ * @count: a parameter
+ * return: returns count
+ */
+
+size_t binary_tree_count_l(const binary_tree_t *tree, size_t count)
 {
 	if (tree == NULL)
-		return count;
+		return (count);
 	count = binary_tree_count_l(tree->left, count);
 	if (tree->left == NULL && tree->right == NULL)
-    	count++;
+		count++;
 	count = binary_tree_count_l(tree->right, count);
-	return count;
+	return(count);
 }
+/*
+ * binary_tree_leaves - a function
+ * @tree: parameter that represents tree
+ * return: returns count
+ */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-  return binary_tree_count_l(tree, 0);
+	return (binary_tree_count_l(tree, 0));
 }
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,16 +1,30 @@
 #include "binary_trees.h"
 
-size_t count_nl(const binary_tree_t *tree,size_t count)
+/**
+ * count_nl - a function that returns count
+ * @tree: paremeter tree
+ * @count: parameter count
+ * Return: returns count
+ */
+
+size_t count_nl(const binary_tree_t *tree, size_t count)
 {
 	if (tree == NULL)
-		return count;
+		return (count);
 	count = count_nl(tree->left, count);
 	if (tree->left != NULL || tree->right != NULL)
-    	count++;
+		count++;
 	count = count_nl(tree->right, count);
-	return count;
+	return (count);
 }
+
+/**
+ * binary_tree_nodes- a function that returns count_nl
+ * @tree: arg that takes tree
+ * Return: returns count_nl
+ */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-return count_nl(tree, 0);
+	return (count_nl(tree, 0));
 }
